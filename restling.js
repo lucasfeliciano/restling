5d64941a19c92c7d1d6caed5df6b6807cae25c02
Lucas Feliciano <lucasjbaff@gmail.com>
2014-07-11 15:52:12 -0300

    Made a function factory to exports

--- restling.js
+++ restling.js
@@ -1,13 +1,19 @@
 'use strict';
 var Q = require('q');
+var _ = require('lodash');
 var restler = require('restler');
 
-exports.get = function(url, params){
-  var d = Q.defer();
-  restler.get(url, params).on('complete', function(data){
-    d.resolve(data);
-  }).on('error', function(err){
-    d.reject(err);
-  });
-  return d.promise;
-};
+var httpVerbs = ['request', 'get', 'post', 'put', 'del', 'head', 'patch'];
+
+// Function that generate the restler wrapper for all the basic http verbs to return promise
+_.forEach(httpVerbs, function(verb){
+  exports[verb] = function(url, options){
+    var d = Q.defer();
+    restler[verb](url, options).on('complete', function(data, response){
+      d.resolve({'data': data, 'response':response});
+    }).on('error', function(err, response){
+      d.reject({'error': err, 'response': response });
+    });
+    return d.promise;
+  };
+});
