f58a9b5c7ae2ff6c1fe9b17e59a12879e93d7fee
Lucas Feliciano <mail@lucasfeliciano.com>
2014-07-24 07:41:38 -0300

    refactor to use bluebird

--- restling.js
+++ restling.js
@@ -1,8 +1,8 @@
 'use strict';
-var Q = require('q');
+
 var _ = require('lodash');
 var restler = require('restler');
-var async = require('async');
+var Promise = require('bluebird');
 
 var httpVerbs = ['request', 'get', 'post', 'put', 'del', 'head', 'patch'];
 var jsonMethods =['json', 'postJson', 'putJson'];
@@ -10,108 +10,109 @@ var jsonMethods =['json', 'postJson', 'putJson'];
 
 // Function that generate the restler wrapper for all the basic http verbs to return promise
 _.forEach(httpVerbs, function(verb){
-  exports[verb] = function(url, options){
-    var d = Q.defer();
-    restler[verb](url, options).on('complete', function(result, response){
-      if (result instanceof Error) {
-        d.reject(result)
-      } else {
-        d.resolve({'data': result, 'response':response});
-      }
-    }).on('error', function(err){
-      d.reject(err);
-    }).on('timeout', function(ms){
-      d.reject({'name': 'Timeout', 'message':'Timeout after '+ms+'ms'});
+  exports[verb] = function (url, options){
+    var request = restler[verb](url, options || {});
+
+    return new Promise(function(resolve, reject) {
+      request
+        .on('complete', function(data, response){
+          if (data instanceof Error) {
+            reject(data);
+          } else {
+            resolve({'data': data, 'response':response});
+          }
+        })
+        .on('success', function(data, response) {
+          resolve({'data': data, 'response': response});
+        })
+        .on('fail', function(data, response) {
+          resolve({'data': data, 'response': response});
+        })
+        .on('error', function(err) {
+          reject(err);
+        })
+        .on('abort', function() {
+          reject(new Promise.CancellationError());
+        })
+        .on('timeout', function() {
+          reject(new Promise.TimeoutError());
+        });
     });
-    return d.promise;
   };
 });
 
 // Function that generate the restler wrapper for all the json methods to return promise
 _.forEach(jsonMethods, function(verb){
-  exports[verb] = function(url, data, options){
-    var d = Q.defer();
-    restler[verb](url, data, options).on('complete', function(result, response){
-      if (result instanceof Error) {
-        d.reject(result)
-      } else {
-        d.resolve({'data': result, 'response':response});
-      }
-    }).on('error', function(err){
-      d.reject(err);
-    }).on('timeout', function(ms){
-      d.reject({'name': 'Timeout', 'message':'Timeout after '+ms+'ms'});
+  exports[verb] = function (url, data, options){
+    var request = restler[verb](url, data, options || {});
+
+    return new Promise(function(resolve, reject) {
+      request
+        .on('complete', function(data, response){
+          if (data instanceof Error) {
+            reject(data);
+          } else {
+            resolve({'data': data, 'response':response});
+          }
+        })
+        .on('success', function(data, response) {
+          resolve({'data': data, 'response': response});
+        })
+        .on('fail', function(data, response) {
+          resolve({'data': data, 'response': response});
+        })
+        .on('error', function(err) {
+          reject(err);
+        })
+        .on('abort', function() {
+          reject(new Promise.CancellationError());
+        })
+        .on('timeout', function() {
+          reject(new Promise.TimeoutError());
+        });
     });
-    return d.promise;
   };
 });
 
 //Function to make parallel async http requests.
-exports.parallelGet = function(requests, finalCallback){
-  var wrappedRequests;
+exports.parallelGet = function (myRequests) {
+  if (_.isArray(myRequests)) {
 
-  wrappedRequests = wrapRequests(requests);
-  var d = Q.defer();
-  async.parallel(wrappedRequests, function(err, results){
-    if(err){
-      q.reject(err);
-    }
-    else {
-      var promisesValue;
-      if(_.isArray(results)) {
-        Q.allSettled(results).then(function(promises){
-          promisesValue = _.map(promises, function(promise){
-            return getPromiseValue(promise);
-          });
-          d.resolve(promisesValue);
-        });
-      }
-      else if (_.isObject(results)){
-        var keys = _.keys(results);
-        var values = _.values(results);
+    return Promise.settle(wrapMyRequests(myRequests)).then(function(settledValues){
+      return getSettledValues(settledValues);
+    });
+  }
+  else if (_.isObject(myRequests)) {
+    var keys = _.keys(myRequests);
+    var values = wrapMyRequests(myRequests);
 
-        Q.allSettled(values).then(function(promises){
-          values = _.map(promises, function(promise){
-            return getPromiseValue(promise);
-          });
-          d.resolve(_.zipObject(keys, values));
-        });
-      }
-    }
-  });
-  return d.promise;
+    return Promise.settle(values).then(function(settledValues) {
+      var resolved = _.zipObject(keys, getSettledValues(settledValues));
+      return  Promise.resolve(resolved);
+    });
+  }
 };
 
-//Auxilar functions
+// Aux Functions
 
-function wrapRequests(requests) {
-  var requestsWrapped;
-  if (_.isArray(requests)) {
-    requestsWrapped = _.map(requests, function(request){
-      return wrapRequestObject(request);
-    });
-  }
-  else if (_.isObject(requests)) {
-    requestsWrapped = _.mapValues(requests, function(request){
-      return wrapRequestObject(request);
-    });
-  }
-  return requestsWrapped;
+function getSettledValues(settledValues) {
+  return _.map(settledValues, function(settledValue){
+    return getPromiseValue(settledValue);
+  });
 }
-
-function wrapRequestObject(request){
-  return function(callback){
-    callback(null, exports.get(request.url, request.options));
-  }
-}
-
 function getPromiseValue(promise) {
-  var pValue;
-  if (promise.state === 'rejected') {
-    pValue = promise.reason;
+  var promiseValue;
+
+  if(promise.isFulfilled()){
+    promiseValue = promise.value();
+  } else if (promise.isRejected()){
+    promiseValue = promise.reason();
   }
-  else if (promise.state === 'fulfilled') {
-    pValue = promise.value;
-  }
-  return pValue;
+  return promiseValue;
+}
+
+function wrapMyRequests(myRequests) {
+  return _.map(myRequests, function(myRequest){
+    return exports.get(myRequest.url, myRequest.options);
+  });
 }
