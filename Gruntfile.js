582db354338b886b17214bf8b9528a4f6fcb95b9
Lucas Feliciano <mail@lucasfeliciano.com>
2015-06-28 06:54:26 -0300

    travis+coveralls

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    coveralls: {
+      // Options relevant to all targets
+      options: {
+        // When true, grunt-coveralls will only print a warning rather than
+        // an error, to prevent CI builds from failing unnecessarily (e.g. if
+        // coveralls.io is down). Optional, defaults to false.
+        force: false
+      },
+
+      your_target: {
+        // LCOV coverage file (can be string, glob or array)
+        src: '.coverage/coverage.lcov',
+        options: {
+          // Any options for just this target
+        }
+      },
+    }
+  });
+
+grunt.loadNpmTasks('grunt-coveralls');
+
+};
